ef896564dead089b49cadbb8a4411fbc21e3fcf4
mnrom2034 <120595976+mnrom2034@users.noreply.github.com>
2024-10-25 04:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -44,4 +45,7 @@ def upload_file():
     return jsonify({"error": "Failed to download file"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is provided
+    app.run(debug=True, host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=5000)
